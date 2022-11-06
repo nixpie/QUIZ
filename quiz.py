a45044776f846e48981476aa10f7857ebfe0993c
nixpie <adriantrojanowski@protonmail.com>
2022-11-06 22:54:57 +0100

    third commit

--- quiz.py
+++ quiz.py
@@ -1,5 +1,23 @@
 from json import load
 
+correct_answers = 0
 with open('quiz.json', encoding='utf8') as questions_file:
     questions = load(questions_file)
-    print(questions)
+
+    for question in questions:
+        print(question['questions'])
+        for index, answer in enumerate(question['answers']):
+            print(index, answer['text'])
+
+        user_answer = int(input('Jak myślisz, która odpowiedź jest prawidłowa? '))
+
+        try:
+            is_correct_answer = question['answers'][user_answer]['correct']
+            if is_correct_answer:
+                correct_answers += 1
+        except IndexError:
+            pass
+
+        print('--' * 10)
+
+print('Poprawnych odpowiedzi', correct_answers)
